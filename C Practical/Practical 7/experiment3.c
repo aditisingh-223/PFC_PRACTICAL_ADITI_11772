f904a704ba63e4f6c1b86708c09651b906ac8d11
Aditi Singh <aditisingh-223@users.noreply.github.com>
2025-12-26 16:44:35 +0530

    Initial commit: C Practical files

---
+++ C Practical/Practical 7/experiment3.c
@@ -0,0 +1,20 @@
+/* Aditi singh */
+/* ERP 11772 */
+#include <stdio.h>
+
+int main() {
+    for (int i = 1; i <= 4; i++) {
+        for (int j = 0; j < i; j++) {
+            printf("%c", 'A' + j);
+        }
+        printf("\n");
+    }
+    return 0;
+}
+/* Output:
+A
+AB
+ABC
+ABCD
+*/
+
---
+++ C Practical/Practical 8/experiment3.c
@@ -0,0 +1,25 @@
+/* Aditi singh */
+/* ERP 11772 */
+#include <stdio.h>
+
+void printTriangle(int n) {
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= i; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+}
+
+int main() {
+    int n = 4;
+    printTriangle(n);
+    return 0;
+}
+/* Output:
+*
+**
+***
+****
+*/
+
